f93ae782e990117cce92b3feb4c19830b3e6214b
Irvin Lejarazo <lejarazoirvin@gmail.com>
2026-05-23 15:24:59 -0500

    Added global styling and homepage layout

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,13 @@
-function Home() {
-  return <h1>World Cup Platform</h1>;  
+function Home () {
+    return (
+        <div className="page-container">
+            <h1>Welcome to the World Cup Dashboard</h1>
+
+            <p>
+                Track teams, players, match statistics, and World Cup History.
+            </p>
+        </div>
+    );
 }
 
 export default Home;
